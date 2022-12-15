f430930780784b509dae02850f570881c310b355
Raheel Farouk <raheelfarouk@gmail.com>
2022-12-15 13:53:52 -0500

    Update files

--- Code/Client/readUDPStream.m
+++ Code/Client/readUDPStream.m
@@ -3,9 +3,10 @@ udpSocket1 = udpport("byte", "IPV4");
 
 %udpSocket2 = udpport("byte", "IPV4");
 %client = tcpclient("raheelfarouk.tplinkdns.com",20003)
+serverAddress = "192.168.88.254";
 
 % write to UDP Server
-write(udpSocket1, "Add Me", "string", "192.168.1.100", 20003);
+write(udpSocket1, "Add Me", "string", "192.168.88.254", 20003);
 %write(client,"Add Me")
 
 %Read data from UDP Server
